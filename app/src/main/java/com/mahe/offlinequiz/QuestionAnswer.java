4298e028d098947f6de31c68ababb2968bc4c403
MAHESH2056 <maheschy2323@gmaill.com>
2023-07-21 13:35:53 +0545

    Final commit

---
+++ app/src/main/java/com/mahe/offlinequiz/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.mahe.offlinequiz;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language",
+            "Where you are watching this video?",
+            "What is the value of the postfix expression 6 3 2 4 + – *?",
+            "Which of the architecture is power efficient?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"1","40","74","-18"},
+            {"RISC","ISA","IANA","CISC"}
+    };
+
+    public static String correctAnswer[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "-18",
+            "RISC"
+    };
+}
